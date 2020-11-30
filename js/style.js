004e81a35d7fcf548495bdfd316bb9d2202912c4
LikeAaron12389 <48329779+LikeAaron12389@users.noreply.github.com>
2020-11-30 23:05:49 +0100

    mergen

---
+++ js/style.js
@@ -0,0 +1,31 @@
+function typeEffect(element, speed) {
+	var text = element.innerHTML;
+	element.innerHTML = "";
+	
+	var i = 0;
+	var timer = setInterval(function() {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+
+// application
+var speed = 100;
+var h1 = document.querySelector('h1');
+
+var delay = h1.innerHTML.length * speed + speed;
+
+// type affect to header
+typeEffect(h1, speed);
+
+
+// type affect to body
+setTimeout(function(){
+  p.style.display = "inline-block";
+  typeEffect(p, speed);
+}, delay);
